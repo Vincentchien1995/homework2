4162548f2d6b393abf9d97f8c2e660e2998e221d
Vincentchien1995 <sheepjessie@gmail.com>
2023-12-05 19:10:30 +0100

    update

--- main.py
+++ main.py
@@ -4,12 +4,13 @@ text = text.replace(',','').replace('.','')
 word=text.split()
 print(len(word))
 
+# #reads a text file containing some English text and counts all words that are:
+# a) at least 4 letters long and
+# b) start with a capital letter or are all capitalized (abbreviations etc.)
 word_count = 0
 for a in word:
-  if len(a)>4:
+  if len(a)>=4 and str.isupper(a) or str.istitle(a):
     word_count += 1
-  return word_count
-
 print(word_count)
 
 
