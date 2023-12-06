41b2f3a9f9c075448380c5258824111fa9334fb4
Vincentchien1995 <sheepjessie@gmail.com>
2023-12-06 17:05:30 +0100

    update

--- main.py
+++ main.py
@@ -1,17 +1,15 @@
-# reads a text file containing some English text and counts all words that are:
-# a) at least 4 letters long and
-# b) start with a capital letter or are all capitalized (abbreviations etc.)
-
+# reads a text file containing some English text and replace special symbol
 f =open('/Users/vincentchien/Desktop/text.txt','r')
 text = f.read()
 text = text.replace(',','').replace('.','')
 word=text.split()
 
-# word_count = 0
-# for a in word:
-#   if len(a)>=4 and str.isupper(a) or str.istitle(a):
-#     word_count += 1
-# print(word_count)
+# count text with at least 4 letters long and start with a capital letter or are all capitalized
+word_count = 0
+for a in word:
+  if len(a)>=4 and str.isupper(a) or str.istitle(a):
+    word_count += 1
+print(word_count)
 
 # Print a list of those words and the number of their occurrences
 word_dic={}
@@ -23,4 +21,16 @@ for i in word:
             word_dic[i] = 1
 print(word_dic)
 
-# Use matplotlib or plotly and generate a bar chart showing the top 10 most common words (same conditions 1a) and 1b) apply) with the number of their occurrences in descending order.
+# sort list then get top 10 words
+w = sorted(word_dic.items(), key=lambda x: x[1], reverse=True)
+top_10 = w[:10]
+print(top_10)
+
+# Use matplotlib or plotly and generate a bar chart
+import matplotlib.pyplot as plt
+words,counts =zip(*top_10)
+plt.bar(words, counts, color = 'orange')
+plt.show()
+
+# use wordcloud
+import wordcloud
